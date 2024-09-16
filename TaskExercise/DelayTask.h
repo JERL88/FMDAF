34edf8d2b5a4d2d1cf12cca9c1655f93b8974f6e
Bryan Lopez <jimenezbryank@gmail.com>
2024-09-15 18:42:52 -0600

    Adding the codes for the HW

--- TaskExercise/DelayTask.h
+++ TaskExercise/DelayTask.h
@@ -1,5 +1,6 @@
 #include <esp_system.h>
 #include <Task.h>
+//Created delay task with ingeritance of Task
 class DelayTask : public Task
 {
     public: 
--- TaskExercise/Task.h
+++ TaskExercise/Task.h
@@ -1,21 +1,42 @@
 #include <esp_system.h>
-
+#include <freertos>
+//Creation of main class Task
 class Task
 {
     protected: 
-    const char taskName;
-    uint32_t stackSize;
-    UBaseType_t priority;
-    TaskHandle_t taskHandle;
+    const char taskName;   //name of task
+    uint32_t stackSize;    //stack size of task
+    UBaseType_t priority;  //priority of task
+    TaskHandle_t taskHandle; //task identifier
     
     public:
-    Task(const char* name, uint32_t stack, UBaseType_t Prio)
+    
+    Task(const char* name, uint32_t stack, UBaseType_t Prio) //task constructor
+    : taskName(name), stackSize(stack), priority(prio), taskHandle(nullptr) 
     {
+
     }
-    ~Task()
+    
+    ~Task() //task destructor 
+    { 
+        stop();
+    }
+    
     virtual void taskFunction()=0
+
+    static void taskFunctionWrapper(void* pvParameters)
+    {
+        // Convert the pointer back to the object
+        Task* task = static_cast<Task*>(pvParameters);
+        // Call the task 
+        task->taskFunction();
+        // Delete the task
+        vTaskDelete(nullptr);
+    }
+
     void start()
     { 
+        xTaskCreate()
     }
     void stop()
     {
